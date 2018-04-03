3e204d05c78d2d29b0eb69295b4b766854e14669
Mikael Jorda <mjorda@stanford.edu>
2018-04-03 10:34:11 -0700

    simple example

---
+++ 00-kuka_joint_hold_pos/controller.cpp
@@ -0,0 +1,138 @@
+// This example application runs a controller for the IIWA
+
+#include "Sai2Model.h"
+#include "redis/RedisClient.h"
+#include "timer/LoopTimer.h"
+
+#include <iostream>
+#include <string>
+#include <tinyxml2.h>
+
+#include <signal.h>
+bool runloop = true;
+void sighandler(int sig)
+{ runloop = false; }
+
+using namespace std;
+using namespace Eigen;
+
+const string robot_file = "../resources/00-kuka_joint_hold_pos/iiwa7.urdf";
+const std::string robot_name = "Kuka-IIWA";
+
+unsigned long long controller_counter = 0;
+
+const bool simulation = true;
+// const bool simulation = false;
+
+// redis keys:
+// - write:
+std::string JOINT_TORQUES_COMMANDED_KEY;
+// - read:
+std::string JOINT_ANGLES_KEY;
+std::string JOINT_VELOCITIES_KEY;
+std::string EE_FORCE_SENSOR_KEY;
+
+int main() {
+	if(simulation)
+	{
+		JOINT_TORQUES_COMMANDED_KEY = "sai2::iiwaForceControl::iiwaBot::actuators::fgc";
+		JOINT_ANGLES_KEY  = "sai2::iiwaForceControl::iiwaBot::sensors::q";
+		JOINT_VELOCITIES_KEY = "sai2::iiwaForceControl::iiwaBot::sensors::dq";
+		EE_FORCE_SENSOR_KEY = "sai2::optoforceSensor::6Dsensor::force_moment";
+	}
+	else
+	{
+		JOINT_TORQUES_COMMANDED_KEY = "sai2::KUKA_IIWA::actuators::fgc";
+		JOINT_ANGLES_KEY  = "sai2::KUKA_IIWA::sensors::q";
+		JOINT_VELOCITIES_KEY = "sai2::KUKA_IIWA::sensors::dq";
+		EE_FORCE_SENSOR_KEY = "sai2::optoforceSensor::6Dsensor::force";
+	}
+
+	// start redis client
+	HiredisServerInfo info;
+	info.hostname_ = "127.0.0.1";
+	info.port_ = 6379;
+	info.timeout_ = { 1, 500000 }; // 1.5 seconds
+	auto redis_client = RedisClient();
+	redis_client.serverIs(info);
+
+	// set up signal handler
+	signal(SIGABRT, &sighandler);
+	signal(SIGTERM, &sighandler);
+	signal(SIGINT, &sighandler);
+
+	// load robots
+	auto robot = new Sai2Model::Sai2Model(robot_file, false);
+
+	// read from Redis
+	redis_client.getEigenMatrixDerived(JOINT_ANGLES_KEY, robot->_q);
+	redis_client.getEigenMatrixDerived(JOINT_VELOCITIES_KEY, robot->_dq);
+
+	////////////////////////////////////////////////
+	///    Prepare the joint controller        /////
+	////////////////////////////////////////////////
+
+	robot->updateModel();
+
+	int dof = robot->dof();
+	VectorXd command_torques = VectorXd::Zero(dof);
+
+	double kp = 25.0;
+	double kv = 5.0;
+
+	VectorXd coriolis = VectorXd::Zero(dof);
+
+	VectorXd initial_position = robot->_q;
+	VectorXd desired_position = initial_position;
+
+	// create a loop timer
+	double control_freq = 1000;
+	LoopTimer timer;
+	timer.setLoopFrequency(control_freq);   // 1 KHz
+	// timer.setThreadHighPriority();  // make timing more accurate. requires running executable as sudo.
+	timer.setCtrlCHandler(sighandler);    // exit while loop on ctrl-c
+	timer.initializeTimer(1000000); // 1 ms pause before starting loop
+
+	// while window is open:
+	while (runloop) {
+
+		// wait for next scheduled loop
+		timer.waitForNextLoop();
+
+		// read from Redis
+		redis_client.getEigenMatrixDerived(JOINT_ANGLES_KEY, robot->_q);
+		redis_client.getEigenMatrixDerived(JOINT_VELOCITIES_KEY, robot->_dq);
+		
+		// update the model 20 times slower (hacky, should use a separate thread)
+		if(controller_counter%20 == 0)
+		{
+			robot->updateModel();
+
+			robot->coriolisForce(coriolis);
+		}
+
+		////////////////////////////// Compute joint torques
+		double time = controller_counter/control_freq;
+
+		command_torques = -kp * (robot->_q - desired_position) - kv * robot->_dq;
+
+		//------ Final torques
+		// command_torques.setZero();
+		redis_client.setEigenMatrixDerived(JOINT_TORQUES_COMMANDED_KEY, command_torques);
+
+		controller_counter++;
+
+	}
+
+    command_torques << 0,0,0,0,0,0,0;
+    redis_client.setEigenMatrixDerived(JOINT_TORQUES_COMMANDED_KEY, command_torques);
+
+    double end_time = timer.elapsedTime();
+    std::cout << "\n";
+    std::cout << "Loop run time  : " << end_time << " seconds\n";
+    std::cout << "Loop updates   : " << timer.elapsedCycles() << "\n";
+    std::cout << "Loop frequency : " << timer.elapsedCycles()/end_time << "Hz\n";
+
+
+    return 0;
+}
