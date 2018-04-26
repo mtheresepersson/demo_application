3c97d370b8dc51e80798be2fed67da1607010cdf
mtheresepersson <thepers@kth.se>
2018-04-25 19:04:14 -0700

    Changed controller

--- 00-kuka_joint_hold_pos/controller.cpp
+++ 00-kuka_joint_hold_pos/controller.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <tinyxml2.h>
 
+// Makes sure that program exits nicely
 #include <signal.h>
 bool runloop = true;
 void sighandler(int sig)
@@ -65,33 +66,105 @@ int main() {
 	auto robot = new Sai2Model::Sai2Model(robot_file, false);
 
 	// read from Redis
-	redis_client.getEigenMatrixDerived(JOINT_ANGLES_KEY, robot->_q);
+	redis_client.getEigenMatrixDerived(JOINT_ANGLES_KEY, robot->_q); // Read from redis and write to robot
 	redis_client.getEigenMatrixDerived(JOINT_VELOCITIES_KEY, robot->_dq);
 
 	////////////////////////////////////////////////
 	///    Prepare the joint controller        /////
 	////////////////////////////////////////////////
 
+	//cout<<"hello"<<endl;
+
 	robot->updateModel();
 
 	int dof = robot->dof();
-	VectorXd command_torques = VectorXd::Zero(dof);
+	
 
-	double kp = 25.0;
+	/*double kp = 25.0;
 	double kv = 5.0;
+	double kvq = 5.0; // Joints*/
+	double kp = 100.0;
+	double kv = 30.0;
+	double kvq = 20.0;
 
 	VectorXd coriolis = VectorXd::Zero(dof);
 
-	VectorXd initial_position = robot->_q;
-	VectorXd desired_position = initial_position;
+	//Position
+	Vector3d endeffector_pos = Vector3d(0.0,0.0,0.025); // Endeffector's position in frame 7
+	//Vector3d initial_xpos;
+	//robot->position(initial_xpos, "link7", endeffector_pos); // Get initial position of link7 (endeffector)
+	//Vector3d desired_xpos = initial_xpos;
+	Vector3d desired_xpos = Vector3d(0.0,0.3,0.2);
+
+
+	//Rotation
+	//Matrix3d initial_xrot;
+	//robot->rotation(initial_xrot, "link7");
+	Matrix3d desired_xrot = Matrix3d::Identity(3,3);
+	desired_xrot << 1,0,0,
+					0,1,0,
+					0,0,-1;
+
+
+	// Linear operational space matrices
+	MatrixXd lambda_pos = MatrixXd::Zero(3,3);
+	MatrixXd Jbar_pos = MatrixXd::Zero(dof,3);
+	MatrixXd N_pos = MatrixXd::Zero(dof,dof);
+
+	// Angular operational space matrices
+	MatrixXd lambda_ang = MatrixXd::Zero(3,3);
+	MatrixXd Jbar_ang = MatrixXd::Zero(dof,3);
+	MatrixXd N_ang = MatrixXd::Zero(dof,dof);
+
+	// Augmented
+	MatrixXd J_ang_aug = MatrixXd::Zero(3, dof);
+	MatrixXd lambda_ang_aug = MatrixXd::Zero(3,3);
+	MatrixXd Jbar_ang_aug = MatrixXd::Zero(dof,3);
+	MatrixXd N_ang_aug = MatrixXd::Zero(dof,dof);
+
+	// Full Jacobian
+	//MatrixXd J = MatrixXd::Zero(6,dof);
+
+	// Linear and angular jacobian
+	MatrixXd Jv = MatrixXd::Zero(3,dof);
+	MatrixXd Jw = MatrixXd::Zero(3,dof);
+
+	// Initialize forces and torques
+	VectorXd F_pos = VectorXd::Zero(3); 
+	VectorXd F_ang = VectorXd::Zero(3); 
+	VectorXd command_torques = VectorXd::Zero(dof);
+
+
+	// Position and orientation
+	Vector3d x_pos = Vector3d::Zero(3);
+	Matrix3d x_rot = Matrix3d::Zero(3,3);
+
+	// Position and Orientation error
+	Vector3d pos_error = Vector3d::Zero(3);
+	Vector3d rot_error = Vector3d::Zero(3);
+	VectorXd error = VectorXd::Zero(6);
+
+	// Velocity
+	Vector3d linVel = Vector3d::Zero(3);
+    Vector3d angVel = Vector3d::Zero(3);
+    VectorXd dx = VectorXd::Zero(6);
+
+    VectorXd gamma0 = VectorXd::Zero(dof);
 
 	// create a loop timer
 	double control_freq = 1000;
 	LoopTimer timer;
 	timer.setLoopFrequency(control_freq);   // 1 KHz
-	// timer.setThreadHighPriority();  // make timing more accurate. requires running executable as sudo.
 	timer.setCtrlCHandler(sighandler);    // exit while loop on ctrl-c
 	timer.initializeTimer(1000000); // 1 ms pause before starting loop
+	//cout << "Hello"<<endl;
+
+	// KEVEN TRAJECTORY
+	/*double max_x = 0.6;
+	double min_x = -0.6;
+	double center_y = 0.3;
+	double move_step = 1.0 / 10000;
+	int direction = 1; // 1 is positive, -1 is negative*/
 
 	// while window is open:
 	while (runloop) {
@@ -114,7 +187,81 @@ int main() {
 		////////////////////////////// Compute joint torques
 		double time = controller_counter/control_freq;
 
-		command_torques = -kp * (robot->_q - desired_position) - kv * robot->_dq;
+		//----------------TRAJECTORIES-----------------------------------
+
+		// Trajectory Half circle in yz-direction
+		/*desired_x(1) = 0.5*cos(M_PI/4*time);
+		desired_x(2) = abs(0.5*sin(M_PI/4*time));*/
+
+		// Trajectory Circle (Position)
+		desired_xpos(0) = 0.5*cos(M_PI/4*time);
+		desired_xpos(1) = 0.5*sin(M_PI/4*time);
+
+		// KEVEN TRAJECTORY
+		// set desired end effector position
+		/*auto x = desired_xpos[0];
+		if (x > max_x) {
+			direction = -1;
+
+		} else if (x < min_x) {
+			direction = 1;
+		}
+		desired_xpos[0] += direction * move_step;
+		x = desired_xpos[0];
+		desired_xpos[2] = center_y + 0.3 * sin(x * 20.);*/
+
+		//----------------------------------------------------------------
+
+
+		// Get current position
+		robot->position(x_pos, "link7", endeffector_pos);
+
+		// Get current rotation
+		robot->rotation(x_rot, "link7");
+
+		// Current velocity
+		robot->linearVelocity(linVel, "link7", endeffector_pos);
+		robot->angularVelocity(angVel, "link7");
+		dx << linVel, angVel;
+
+		// Get current error
+		pos_error << x_pos - desired_xpos;
+		robot->orientationError(rot_error, desired_xrot, x_rot);
+		error << pos_error, rot_error;
+		
+
+		//robot->J_0(J, "link7", endeffector_pos); // Populate Jacobian
+		robot->Jv(Jv, "link7", endeffector_pos); // Populate Jacobian
+		robot->Jw(Jw, "link7"); // Populate Jacobian
+
+		// Mass
+		lambda_pos = (Jv*robot->_M_inv*Jv.transpose()).inverse();
+		//lambda_ang = (Jw*robot->_M_inv*Jw.transpose()).inverse();
+
+		// Inverse Jacobians
+		Jbar_pos = (robot->_M_inv)*Jv.transpose()*lambda_pos;
+		Jbar_ang = (robot->_M_inv)*Jw.transpose()*lambda_ang;
+
+
+		// Nullspace
+		N_pos << MatrixXd::Identity(dof,dof)-Jbar_pos*Jv;
+		N_ang << MatrixXd::Identity(dof,dof)-Jbar_ang*Jw;
+
+		// Augmented
+		J_ang_aug = Jw*N_pos;
+		lambda_ang = (J_ang_aug*robot->_M_inv*J_ang_aug.transpose()).inverse();
+		Jbar_ang_aug = (robot->_M_inv)*J_ang_aug.transpose()*lambda_ang_aug;
+
+		N_ang_aug = N_pos.transpose()*(MatrixXd::Identity(dof,dof) - J_ang_aug.transpose()*Jbar_ang_aug.transpose());
+		//F = lambda*(-kp*error - kv*dx);
+		F_pos = lambda_pos*(-kp*pos_error - kv*linVel);
+		F_ang = lambda_ang*(-kp*rot_error - kv*angVel);
+		//F_ang = lambda_ang_aug*(-kp*rot_error - kv*angVel);
+		gamma0 = -kvq*robot->_M*robot->_dq; // Damp joint motions, DO WE HAVE ENOUGH DOF FOR THIS?
+
+		command_torques = Jv.transpose()*F_pos + N_pos.transpose()*Jw.transpose()*F_ang; //+ N_pos.transpose()*N_ang.transpose()*gamma0;
+		//command_torques = Jv.transpose()*F_pos + J_ang_aug.transpose()*F_ang + N_ang_aug*gamma0;
+		//cout<<"hello"<<endl;
 
 		//------ Final torques
 		// command_torques.setZero();
